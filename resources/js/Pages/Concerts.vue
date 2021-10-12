549090b73cabda84b13939922ae80745a2f0278d
John Mancuso <johnmancuso79@gmail.com>
2021-10-12 14:16:19 -0700

    Update

--- resources/js/Pages/Concerts.vue
+++ resources/js/Pages/Concerts.vue
@@ -7,19 +7,19 @@
         <!-- 
             Content 
         -->
-  <!--<div class="bg-feat">
+  <div class="bg-feat">
     <div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
-      <h2 class="text-3xl font-extrabold text-white sm:text-4xl">
-        <span class="block">Josh Heinrichs</span>
-        <span class="block">@ Tiki Bar Sunday, October 10th</span>
+      <h2 class="text-3xl font-extrabold text-black sm:text-4xl">
+        <span class="block">Crazy Town & Green Jelly</span>
+        <span class="block">@ Tiki Bar Saturday, November 27th</span>
       </h2>
-      <p class="mt-4 text-lg leading-6 text-white">Tickets are available online or at the door.</p>
-      <a href="/concert/josh-heinrichs-2074377681" class="mt-8 w-full inline-flex items-center justify-center px-5 py-3  text-base    bg-primary text-white font-extrabold  sm:w-auto">
+      <p class="mt-4 text-lg leading-6 text-black">Tickets on sale soon.</p>
+      <!--<a href="/concert/josh-heinrichs-2074377681" class="mt-8 w-full inline-flex items-center justify-center px-5 py-3  text-base    bg-primary text-white font-extrabold  sm:w-auto">
         Tickets & Info
-      </a>
+      </a>-->
     </div>
-    
-  </div>-->
+
+  </div>
         <div class="container mx-auto bg-white p-4 mt-4">
             <!-- Not Loading -->
             <div v-if="isLoading === false">
@@ -185,6 +185,7 @@
 
 <style>
 .bg-feat {
-    background: #202020;
+    background: #8a96fd;
+
 }
 </style>
