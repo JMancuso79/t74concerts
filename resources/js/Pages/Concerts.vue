d57b8e4d96b0ce408bb65c87a75b5d6f3b59129c
JMancuso79 <johnmancuso79@gmail.com>
2022-05-16 17:12:57 -0700

    Update

--- resources/js/Pages/Concerts.vue
+++ resources/js/Pages/Concerts.vue
@@ -19,7 +19,7 @@
                 <div class="">
 
                     <div class="">
-                        <!-- Feature -->
+                        <!-- Feature 
                         <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 mb-6">
                             <a href="https://tower74concerts.com/concert/ital-vibes-album-release-1070187023">
                                 <img src="https://artistwavesoftware.s3-us-west-1.amazonaws.com/posts/625caba6f148a.jpeg" class="w-full" />
@@ -35,7 +35,7 @@
                                     Buy Tickets
                                 </a>
                             </p>
-                        </div>  
+                        </div> --> 
                         <div v-if="isLoading === false">
                             <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 mb-4">
                                 <div class="mb-6">
