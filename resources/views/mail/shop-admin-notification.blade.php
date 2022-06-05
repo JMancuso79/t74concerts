830c12dcae15b230af09c992bd76fbb80f9873bc
JMancuso79 <johnmancuso79@gmail.com>
2022-06-05 12:51:24 -0700

    Update

--- resources/views/mail/shop-admin-notification.blade.php
+++ resources/views/mail/shop-admin-notification.blade.php
@@ -5,53 +5,91 @@
         body {
             background: #ffffff;
             font-family: 'Open Sans', sans-serif;
-            color:#202020;
+            color:#1F2937;
         }
         .bg-white {
             background: #ffffff;  
         }
         .bg-grey {
-            background: #eeeeee;
+            background: #1F2937;
         }
-        .p-5 {
-            padding: 5px;
+        .p-4 {
+            padding: 15px;
         }
-        .mb-5 {
+        .mb-4 {
+            margin-bottom:15px;
+        }
+        .mb-2 {
             margin-bottom:5px;
         }
         .text-capitalize {
             text-transform: capitalize;
         }
+        .center {
+            text-align:center!important;
+        }
     </style>
 </head>
 <body>
-    <div class="bg-grey">
-        <div class="p-5">
-            <h1 class="text-capitalize">New Order From Shop!</h1>
+    <div style="width:100%; max-width:640px; margin:0 auto; border: 2px solid #1F2937;">
+        <div class="bg-grey" >
+            <div class="p-4 center">
+                <img src="https://tower74concerts.com/images/t74-white-wp.png" style="max-width:180px;" width="100%" />
+            </div>
         </div>
-        <!-- Thank You -->
-        <div class="bg-white p-5 mb-5">
-            <h3 class="text-capitalize">Customer</h3>
-            <p>Customer info</p>
-        </div>
-        <!-- Details -->
-        <div class="bg-white p-5 mb-5">
-            <h3 class="text-capitalize">About The Purchase</h3>
-            <p>Loop through products here</p>
-        </div>
-        <!-- Tickets -->
-        <div class="bg-white p-5 mb-5">
-            <h3>Total</h3>
-            <p>
-                Total goes here 
-            </p>
-        </div>
-        <!-- Website -->
-        <div class="p-5">
-            <p>
-                <a href="https://tower74concerts.com">https://tower74concerts.com</a>
-            </p>
-        </div>
+        <div class="bg-white p-4 mb-4 ">
+            <div class="center" style="border-bottom: 1px solid #eeeeee;">
+                <h3>New Order Confirmation</h3>
+            </div>
+            <!-- Tickets -->
+            <div class="mb-4">
+                <h3>Customer</h3>
+                <p>
+                    {{$notification['customer']['name']}} 
+                </p>
+                <p>
+                    {{$notification['customer']['email']}} 
+                </p>
+                <p>
+                    {{$notification['customer_details']['street_address']}} {{$notification['customer_details']['unit']}} 
+                </p>
+                <p>
+                    {{$notification['customer_details']['city']}}, {{$notification['customer_details']['state']}} {{$notification['customer_details']['zip']}} 
+                </p>
+            </div>
+            <!-- Details -->
+            <div class="mb-4">
+
+                @foreach ($notification['products'] as $product)
+                    <div style="border-bottom: 1px solid #eeeeee; padding-bottom:5px;" class="mb-2">
+                        <p>
+
+                            <img src="https://tower74concerts.com/storage/images/628d5902e11b8_back-tee-1.jpg" style="max-width:280px;" width="100%" />
+                        </p>
+                        <p style="margin-top:5px;" class="text-capitalize">
+                            <strong>{{ $product['name'] }}</strong> {{ $product['color'] }} ({{ $product['quantity'] }}  )
+                            <br>Size: {{ $product['size'] }} 
+                        </p>
+                    </div>
+                @endforeach
+            </div>
+
+
+            <!-- Contact -->
+            <div class="mb-4">
+                <h3 class="text-capitalize">Contact</h3>
+                <p>
+                    If you have questions about your order, please do not reply to this email. 
+                </p>
+                <p><a href="https://tower74concerts.com/contact">Click here to contact us</a></p>
+            </div>
+            <!-- Website -->
+            <div class="mb-4">
+                <p>
+                    <a href="https://tower74concerts.com">https://tower74concerts.com</a>
+                </p>
+            </div>
+        </div> 
     </div>            
 </body>
 </html>
--- resources/views/mail/shop-customer-notification.blade.php
+++ resources/views/mail/shop-customer-notification.blade.php
@@ -31,29 +31,33 @@
     </style>
 </head>
 <body>
-    <div style="width:100%; max-width:640px; margin:0 auto; border: 2px solid;">
+    <div style="width:100%; max-width:640px; margin:0 auto; border: 2px solid #1F2937;">
         <div class="bg-grey" >
             <div class="p-4 center">
                 <img src="https://tower74concerts.com/images/t74-white-wp.png" style="max-width:180px;" width="100%" />
             </div>
         </div>
-        <div class="bg-white p-4">
-            <div class="mb-4 center">
+        <div class="bg-white p-4 mb-4 ">
+            <div class="center" style="border-bottom: 1px solid #eeeeee;">
                 <h3>Order Confirmation</h3>
+                <!-- Thank You -->
+                <div>
+                    <p>Your support helps us book more concerts which keeps artists working and creates revenue for venues!</p>
+                </div>
             </div>
             <!-- Details -->
             <div class="mb-4">
                 <h3 class="text-capitalize">About Your Purchase</h3>
 
                 @foreach ($notification['products'] as $product)
-                    <div style="border-bottom: 1px solid #1F2937; padding-bottom:5px;" class="mb-2">
+                    <div style="border-bottom: 1px solid #eeeeee; padding-bottom:5px;" class="mb-2">
                         <p>
 
                             <img src="https://tower74concerts.com/storage/images/628d5902e11b8_back-tee-1.jpg" style="max-width:280px;" width="100%" />
                         </p>
                         <p style="margin-top:5px;" class="text-capitalize">
-                            <strong>{{ $product['name'] }}</strong>
-                            <br>{{ $product['quantity'] }} {{ $product['color'] }} {{ $product['size'] }} 
+                            <strong>{{ $product['name'] }}</strong> {{ $product['color'] }} ({{ $product['quantity'] }}  )
+                            <br>Size: {{ $product['size'] }} 
                         </p>
                     </div>
                 @endforeach
@@ -65,9 +69,21 @@
                     If your products are in stock they ship within two business days of receiving the order. If you purchased an item that is out of stock it will ship within two weeks. 
                 </p>
             </div>
-            <!-- Thank You -->
+            <!-- Tickets -->
             <div class="mb-4">
-                <p>Your support helps us book concerts which creates revenue for venues and keeps artists working!</p>
+                <h3>Customer Details</h3>
+                <p>
+                    {{$notification['customer']['name']}} 
+                </p>
+                <p>
+                    {{$notification['customer']['email']}} 
+                </p>
+                <p>
+                    {{$notification['customer_details']['street_address']}} {{$notification['customer_details']['unit']}} 
+                </p>
+                <p>
+                    {{$notification['customer_details']['city']}}, {{$notification['customer_details']['state']}} {{$notification['customer_details']['zip']}} 
+                </p>
             </div>
             <!-- Contact -->
             <div class="mb-4">
