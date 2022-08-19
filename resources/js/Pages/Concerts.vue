71dfcbbda9a25ad7f2e28e70e5d3640f2f59305b
JMancuso79 <johnmancuso79@gmail.com>
2022-08-19 14:38:54 -0700

    Update

--- resources/js/Pages/Concerts.vue
+++ resources/js/Pages/Concerts.vue
@@ -19,7 +19,7 @@
                 <div class="">
 
                     <div class="">
-                        <!-- Feature -->   
+                        <!-- Feature   
                         <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 mb-6">
                             <a href="https://tower74concerts.com/concert/tomorrows-bad-seeds-1331628688">
                                 <img src="https://artistwavesoftware.s3-us-west-1.amazonaws.com/posts/62b9992dad863.jpeg" class="w-full" />
@@ -35,7 +35,7 @@
                                     Buy Tickets
                                 </a>
                             </p>
-                        </div> 
+                        </div> --> 
                         <div v-if="isLoading === false">
                             <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 mb-4">
                                 <div class="mb-6">
